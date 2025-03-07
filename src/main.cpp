cd5c222127c9310291fb71ae9ac5ba301041b1af
Joe Marshall <joe.marshall@nottingham.ac.uk>
2025-03-07 16:50:24 +0000

    colour picker works

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <lvgl.h>
 #include <esp_timer.h>
 
+LV_IMAGE_DECLARE(wheel);
+LV_IMAGE_DECLARE(wheel_pressed);
+
 constexpr int32_t HOR_RES=320;
 constexpr int32_t VER_RES=240;
 
@@ -24,17 +27,31 @@ uint32_t my_tick_function() {
   return (esp_timer_get_time() / 1000LL);
 }
 
+int lastX=-1;
+int lastY=-1;
+
 void my_touchpad_read(lv_indev_t * drv, lv_indev_data_t * data) {
   M5.update();
   auto count = M5.Touch.getCount();
 
   if ( count == 0 ) {
     data->state = LV_INDEV_STATE_RELEASED;
+    lastX=-1;
+    lastY=-1;
   } else {
     auto touch = M5.Touch.getDetail(0);
-    data->state = LV_INDEV_STATE_PRESSED; 
-    data->point.x = touch.x;
-    data->point.y = touch.y;
+    if(touch.y>=VER_RES){
+      data->state = LV_INDEV_STATE_PRESSED; 
+      data->point.x = touch.x;
+      data->point.y = VER_RES-1;
+
+    }else{
+      data->state = LV_INDEV_STATE_PRESSED; 
+      data->point.x = touch.x;
+      data->point.y = touch.y;
+    }
+    lastX=data->point.x;
+    lastY=data->point.y;
   }
 }
 
@@ -45,19 +62,52 @@ static void event_cb(lv_event_t *e)
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t *label = reinterpret_cast<lv_obj_t *>(lv_event_get_user_data(e));
 
+  char text[64];
+
+
   switch (code)
   {
+    case LV_EVENT_PRESSING:
+    if(lastX!=-1){
+      
+      float offsetSq = (lastX-160)*(lastX-160)+(lastY-120)*(lastY-120);
+      if(offsetSq>99.0*99.0){
+        // shrink to 
+        float ox = lastX-160;
+        float oy = lastY-120;
+        float scale = 99.0/sqrt(offsetSq);
+        ox*=scale;
+        oy*=scale;
+        ox+=160.0;
+        oy+=120.0;
+        lastX=ox;
+        lastY=oy;
+      }
+
+      uint16_t *rgb565Data = (uint16_t*)wheel.data;
+      uint16_t rgb = rgb565Data[lastX+lastY*320];
+      int r,g,b;
+      r=(rgb>>8)&0b11111000;
+      g=(rgb>>2)&0b11111100;
+      b=(rgb<<3)&0b11111000;
+      snprintf(text,64,"Button touching at %d %d (%d %d %d)",lastX,lastY,r,g,b);
+      lv_label_set_text(label, text);
+      // move crosshairs to point here
+  
+    }
+    break;
+
   case LV_EVENT_PRESSED:
-    lv_label_set_text(label, "The last button event:\nLV_EVENT_PRESSED");
+//    lv_label_set_text(label, "The last button event:\nLV_EVENT_PRESSED");
     break;
   case LV_EVENT_CLICKED:
-    lv_label_set_text(label, "The last button event:\nLV_EVENT_CLICKED");
+//    lv_label_set_text(label, "The last button event:\nLV_EVENT_CLICKED");
     break;
   case LV_EVENT_LONG_PRESSED:
-    lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED");
+//    lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED");
     break;
   case LV_EVENT_LONG_PRESSED_REPEAT:
-    lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED_REPEAT");
+//    lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED_REPEAT");
     break;
   default:
     break;
@@ -85,18 +135,34 @@ void lv_example_event_2(void)
   lv_label_set_text(btn_label, "Click me!");
   lv_obj_center(btn_label);
 
-  LV_IMAGE_DECLARE(wheel);
-  LV_IMAGE_DECLARE(wheel_pressed);
+
+
   lv_obj_t * img;
   img = lv_imgbtn_create(lv_scr_act() );
 //  img = lv_image_create(lv_screen_active());
   lv_imgbtn_set_src(img, LV_IMGBTN_STATE_RELEASED,NULL,&wheel,NULL);
   lv_imgbtn_set_src(img, LV_IMGBTN_STATE_PRESSED,NULL,&wheel_pressed,NULL);
   lv_obj_center(img);
-//  lv_obj_align(img, NULL,LV_ALIGN_CENTER, 0, 0);
+
+  LV_IMAGE_DECLARE(uparrow);
+  LV_IMAGE_DECLARE(downarrow);
+  lv_obj_t * up;
+  up = lv_imgbtn_create(lv_scr_act() );
+  lv_imgbtn_set_src(up, LV_IMGBTN_STATE_RELEASED,NULL,&uparrow,NULL);
+  lv_imgbtn_set_src(up, LV_IMGBTN_STATE_PRESSED,NULL,&uparrow,NULL);
+  lv_obj_align(up, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+
+  lv_obj_t * down;
+  down = lv_imgbtn_create(lv_scr_act() );
+  lv_imgbtn_set_src(down, LV_IMGBTN_STATE_RELEASED,NULL,&downarrow,NULL);
+  lv_imgbtn_set_src(down, LV_IMGBTN_STATE_PRESSED,NULL,&downarrow,NULL);
+  lv_obj_align(down, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+
 
   lv_obj_t *info_label = lv_label_create(lv_screen_active());
   lv_label_set_text(info_label, "The last button event:\nNone");
+  lv_obj_set_style_text_color(info_label,LV_COLOR_MAKE(255, 255, 255),LV_PART_MAIN);
+
 
   lv_obj_add_event_cb(img, event_cb, LV_EVENT_ALL, info_label);
 }
